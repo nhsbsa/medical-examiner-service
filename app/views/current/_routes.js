d758d60cbdee1239e48993223129095c018cbdb4
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2023-05-26 13:45:15 +0100

    static case details

--- app/views/current/_routes.js
+++ app/views/current/_routes.js
@@ -4,4 +4,44 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// location of death
+router.post(/location-of-death/, (req, res) => {
+
+    const locationOfDeath = req.session.data['locationOfDeathType']
+
+    if (locationOfDeath == 'hospital-acute') {
+        res.redirect('hospital-acute-setting')
+    } else {
+        res.redirect('community-non-acute')
+    }
+
+})
+
+// Discussion with the attending practitioner about the cause of death
+router.post(/ap-prop-cod-discussion/, (req, res) => {
+
+    const needDiscussion = req.session.data['ap-prop-cod-discussion']
+
+    if (needDiscussion == 'yes') {
+        res.redirect('record-discussion-ap')
+    } else {
+        res.redirect('no-discussion-ap')
+    }
+
+})
+
+// Notification method
+router.post(/notification-method/, (req, res) => {
+
+    const usingServiceToNotiftyCoroner = req.session.data['internalNotification']
+
+    if (usingServiceToNotiftyCoroner == 'yes') {
+        res.redirect('me-comms-coroner')
+    } else {
+        res.redirect('notification-details')
+    }
+
+})
+
+
 module.exports = router;
