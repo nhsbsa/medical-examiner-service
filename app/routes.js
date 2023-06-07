98ef868a8e77310d9e6515debe3bbf4697c3ac17
Dean Cobb <51478242+nhsbsa-dean@users.noreply.github.com>
2023-06-07 16:24:43 +0100

    Created V13

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Current version routes file - matching as close as possible to live
+router.use('/current', require('./views/current/_routes'));
+
 // =======================================
 // Version Routes Files Below
 // =======================================
 
-router.use('/current', require('./views/current/_routes'));
+router.use('/v13', require('./views/v13/_routes'));
+
+
 
 module.exports = router;
