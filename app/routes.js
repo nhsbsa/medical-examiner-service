479c03be090c29bfdf847836279164b886328011
Richard Payne <richy_payne@hotmail.co.uk>
2024-01-08 09:44:02 +0000

    updating global routing file for v15

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,6 @@ router.use('/current', require('./views/current/_routes'));
 
 router.use('/v13', require('./views/v13/_routes'));
 router.use('/v14', require('./views/v14/_routes'));
-
-
+router.use('/v15', require('./views/v15/_routes'));
 
 module.exports = router;
