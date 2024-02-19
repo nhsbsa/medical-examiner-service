da6f63106a3eb6959ca635b1fde1e6b45fc5e3b1
Richard Payne <richy_payne@hotmail.co.uk>
2024-02-19 09:41:23 +0000

    Update routes.js
    fix

--- app/routes.js
+++ app/routes.js
@@ -20,8 +20,5 @@ router.use('/v14', require('./views/v14/_routes'));
 router.use('/v15', require('./views/v15/_routes'));
 router.use('/v16', require('./views/v16/_routes'));
 router.use('/v17', require('./views/v17/_routes'));
-router.use('/v18', require('./views/v18/_routes'));
-router.use('/v19', require('./views/v19/_routes'));
-router.use('/v20', require('./views/v20/_routes'));
 
 module.exports = router;
