129b98221c75e55c55445434ea03aaffb78d6839
Richard Payne <richy_payne@hotmail.co.uk>
2024-02-23 10:43:58 +0000

    testing pulling back new case onto dashboard

--- app/views/v17/_routes.js
+++ app/views/v17/_routes.js
@@ -9,9 +9,11 @@ const router = express.Router();
 // CREATE NEW CASE
 // ========================================================================
 
+
 // Deceased person's details
 router.post(/create-new-case/, (req, res) => {
 
+    req.session.data['create-new-case-section'] = 'complete'
     res.redirect('assign-case')
 
 })
