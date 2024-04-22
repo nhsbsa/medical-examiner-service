a8278164206954ee4dae46f968568b181f74d309
Richard Payne <richy_payne@hotmail.co.uk>
2024-04-22 16:32:56 +0100

    static note pages in v19

--- app/routes.js
+++ app/routes.js
@@ -21,5 +21,6 @@ router.use('/v15', require('./views/v15/_routes'));
 router.use('/v16', require('./views/v16/_routes'));
 router.use('/v17', require('./views/v17/_routes'));
 router.use('/v18', require('./views/v18/_routes'));
+router.use('/v19', require('./views/v18/_routes'));
 
 module.exports = router;
