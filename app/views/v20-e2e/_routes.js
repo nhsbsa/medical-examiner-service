9914f7e181eca051b1255ef23e4dc57db2c62ebf
StuBamforth <snbamforth@yahoo.com>
2024-05-30 16:37:34 +0100

    Further work on case details flow.

--- app/views/v20-e2e/_routes.js
+++ app/views/v20-e2e/_routes.js
@@ -48,31 +48,38 @@ router.post(/additional-details/, (req, res) => {
 
 })
 
+
+
+
 // location of death
 router.post(/location-of-death/, (req, res) => {
 
     const locationOfDeath = req.session.data['locationOfDeathType']
 
     if (locationOfDeath == 'hospital-acute') {
-        res.redirect('hospital-acute-setting')
+        res.redirect('hospital-acute-setting-sp11')
     } else {
-        res.redirect('community-non-acute')
+        res.redirect('community-non-acute-sp11')
     }
 
 })
 
 
-// location of death
 
+// Branching example
+router.post('/examples/branching/answer', (req, res) => {
+  // Make a variable and give it the value from 'know-nhs-number'
+  const nhsNumber = req.session.data['know-nhs-number'];
 
-
-router.post('/location-of-death-check', (req, res) => {
-    if(req.session.data['location-of-death-check'] == 'Hospital'){
-        res.redirect('hospital-acute-setting-sp11')
-    } else if(req.session.data['location-of-death-check'] == 'Community or non-acute setting'){
-        res.redirect('community-non-acute-sp11')
-    } 
-  });
+  // Check whether the variable matches a condition
+  if (nhsNumber === 'Yes') {
+    // Send user to next page
+    res.redirect('/docs/examples/branching/answer-yes');
+  } else {
+    // Send user to ineligible page
+    res.redirect('/docs/examples/branching/answer-no');
+  }
+});
 
 
 
